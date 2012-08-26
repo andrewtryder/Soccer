bbb75a2952535fbca2c8c190d24c4dde6a2b2eca
spline <spline@spline.com>
2012-08-26 13:23:30 -0400

    Move leagues into internal function so we can use elsewhere. Fix utf-8 encoding on match string.

--- plugin.py
+++ plugin.py
@@ -32,25 +32,38 @@ class Soccer(callbacks.Plugin):
         import base64
         return base64.b64decode(string)
 
+    def _validleagues(self, league=None):
+        """Return string containing league string if valid, 0 if error. If no league given, return leagues as keys of tuple."""
+        leagues = { 'MLS':'usa.1', 'EPL':'eng.1', 'LaLiga':'esp.1',
+                    'SerieA':'ita.1', 'Bundesliga':'ger.1', 'Ligue1':'fra.1',
+                    'Eredivise':'ned.1', 'LigaMX':'mex.1'
+                  }
+        
+        if league is None:
+            return leagues.keys() # return the keys here for an list to display.
+        else:
+            if league not in leagues:
+                return "0" # to parse an error.
+            else:
+                return leagues[league]
+            
     ####################
     # Public Functions #
     ####################
+            
     
     def soccer(self, irc, msg, args, optleague):
         """[league]
         Display live/completed scores for various leagues.
         """
         
-        leagues = { 'MLS':'usa.1', 'EPL':'eng.1', 'LaLiga':'spa.1',
-                    'SerieA':'ita.1', 'Bundesliga':'ger.1', 'Ligue1':'fra.1',
-                    'Eredivise':'ned.1', 'LigaMX':'mex.1'
-                 }
+        leagueString = self._validleagues(league=optleague)
         
-        if optleague not in leagues:
-            irc.reply("Must specify league. Leagues is one of: %s" % leagues.keys())
+        if leagueString == "0":
+            irc.reply("Must specify league. Leagues is one of: %s" % (self._validleagues(league=None)))
             return
 
-        url = self._b64decode('aHR0cDovL20uZXNwbi5nby5jb20vc29jY2VyL3Njb3JlYm9hcmQ/') + 'leagueTag=%s&lang=EN&wjb=' % (leagues[optleague])
+        url = self._b64decode('aHR0cDovL20uZXNwbi5nby5jb20vc29jY2VyL3Njb3JlYm9hcmQ/') + 'leagueTag=%s&lang=EN&wjb=' % (leagueString)
     
         try:
             req = urllib2.Request(url)
@@ -65,21 +78,25 @@ class Soccer(callbacks.Plugin):
         append_list = []
 
         for div in divs:
-            if div.find('div', attrs={'style':'white-space: nowrap;'}): # <div style="white-space: nowrap;">
+            if div.find('div', attrs={'style':'white-space: nowrap;'}):
                 match = div.find('div', attrs={'style':'white-space: nowrap;'})
                 if match:
-                    match = match.getText()
+                    match = match.getText().encode('utf-8') # do string formatting/color below. Ugly but it works.
                     match = match.replace('Final -',ircutils.mircColor('FT', 'red') + ' -')
                     match = match.replace('Postponed -',ircutils.mircColor('PP', 'yellow') + ' -')
                     match = match.replace('(ESPN, UK)','').replace('(ESPN3)','').replace(' ET','').replace(' CT','').replace(' PT','')
-                    append_list.append(str(match).strip().encode('utf-8'))
+                    # 17' - Osasuna 1-0 Barcelona | 2:00 PM - Getafe vs Real Madrid
+                    append_list.append(str(match).strip())
             
         if len(append_list) > 0:
             descstring = string.join([item for item in append_list], " | ")
             irc.reply(descstring)
         else:
-            irc.reply("I did not find any matches going on for: %s" % optleague)          
+            irc.reply("I did not find any matches going on for: %s" % leagueString)          
     soccer = wrap(soccer, [('somethingWithoutSpaces')])
+    
+    
+    #def soccerstats(self, irc, msg, args)
 
 Class = Soccer
 
