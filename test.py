d1e6a3f1480beda69531cf014b7bc4b1e128847a
spline <reticulatingspline@github.com>
2014-10-11 16:35:38 -0400

    Add in tests.

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 ###
-# Copyright (c) 2013, spline
+# Copyright (c) 2013-2014, spline
 # All rights reserved.
 #
 #
@@ -9,6 +9,17 @@ from supybot.test import *
 
 class SoccerTestCase(PluginTestCase):
     plugins = ('Soccer',)
+
+    def testSoccer(self):
+        conf.supybot.plugins.Soccer.disableANSI.setValue('True')
+        # fifarankings, soccer, soccerformation, soccerlineup, soccerstats, and soccertable
+        self.assertRegexp('fifarankings', 'FIFA Rankings')
+        self.assertNotError('soccer epl')
+        self.assertNotError('soccerlineup manchester united')
+        self.assertNotError('soccerstats epl goals')
+        self.assertNotError('soccertable epl')
+        
+
 
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
